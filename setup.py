e643c2f7b03bf026d227b929a41f7d9e42d53c13
Yashaswini3377 <yashukurra4@gmail.com>
2025-08-07 22:58:21 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information-retriving-system',
+    version= '0.0.0',
+    author= 'Yashaswini',
+    author_email= 'kyashaswini228@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
